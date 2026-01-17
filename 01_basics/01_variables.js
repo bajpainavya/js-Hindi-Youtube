c16ec4fd488b5fdf739c6c583d91c3352ea73abd
bajpainavya <bajpainavya31@gmail.com>
2026-01-17 12:25:49 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId= 143535;
+
+let accountEmail="bajpainavya53@MediaList.com;"
+
+var acountPassword ="1234";
+
+acoountCity="bengluru";
+
+// const accountId=5464546;
+var acountPassword ="1654";
+
+let classStudent; 
+let classStudents 
+
+console.table([accountId, acountPassword ,classStudent,classStudents]);
+
+// prefer not to use var beacuse of block scope..
+// const can't be redeclared nor reused
+// let cant be redeclared..can be reasign
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+// number =>2 to power 53
+// bigint
+// string=>""
+// boolean=>true/false
+// null =>datatype..standalone value bhi h ...iska type..object h ok
+// undefined means variable declare kiya..bt value ni di..."meansabhi value assign ni ki gyi h
+
+
+
+// object
+console.log(typeof null);
+console.log(typeof undefined); 
+// undefined hi ayega
